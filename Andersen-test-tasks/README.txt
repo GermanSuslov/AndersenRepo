9e890be0e8640d5620f4736dfa63325c16af7a6c
GermanSuslov <germangame94@gmail.com>
2023-02-06 12:55:08 +0400

    Andersen test tasks

---
+++ Andersen-test-tasks/README.txt
@@ -0,0 +1,8 @@
+Чтобы запустить jar, нужно открыть "Anderssen-test-tasks\out\artifacts" в командной строке.
+
+Ввести "java -jar task1-more-than-seven.jar".
+Ввести "java -jar task2-hello-slava.jar".
+Ввести "java -jar task3-divisor-three.jar".
+Ввести "java -jar task4-brackets.jar".
+
+java version "11.0.17"
---
+++ Andersen-test-tasks/src/task3DivisorThree/DivisorThree.java
@@ -0,0 +1,16 @@
+package task3DivisorThree;
+
+import java.util.Scanner;
+
+public class DivisorThree {
+
+    public static void main(String[] args) {
+        System.out.println("Введите массив чисел через пробел");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        input.trim();
+        DivisionChecker divisionChecker = new DivisionChecker(3);
+        divisionChecker.printDivisibleNumbers(input);
+        scanner.close();
+    }
+}
